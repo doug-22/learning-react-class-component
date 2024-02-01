2d40e6d82588ca61241d8ad740cbde7cbea384a8
Douglas <douglas.aguiar410@gmail.com>
2024-02-01 15:41:46 -0300

    refactor/private router

--- src/pages/utils/PrivateRoute.js
+++ src/pages/utils/PrivateRoute.js
@@ -4,11 +4,7 @@ import { Navigate } from 'react-router-dom';
 class PrivateRoute extends Component {
   render() {
     const { isAuthenticated, component: Component } = this.props;
-
-    if (isAuthenticated) {
-      return <Component />;
-    }
-    return <Navigate to={{ pathname: '/' }} />;
+    return isAuthenticated ? <Component /> : <Navigate to="/" />;
   }
 }
 
