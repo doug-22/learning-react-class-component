6ee9ce8e7ea90a748a606ecf7d24ad8f93bc2461
Douglas <douglas.aguiar410@gmail.com>
2024-02-01 18:25:50 -0300

    feat/create divider component

---
+++ src/components/Divider/index.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import './styles.sass';
+
+class Divider extends Component {
+  render() {
+    const { text } = this.props;
+    return <div className="wrapper-divider">{text && <span>{text}</span>}</div>;
+  }
+}
+
+Divider.propTypes = {
+  text: PropTypes.string
+};
+
+export default Divider;
