01b94afdca6b11c603202ffd3ca0683868c0f70e
Douglas <douglas.aguiar410@gmail.com>
2024-02-19 20:43:18 -0300

    refactor/login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -4,6 +4,8 @@ import Input from '../../components/Input';
 import { AiFillGithub } from 'react-icons/ai';
 import './styles.sass';
 import Divider from '../../components/Divider';
+import AuthService from '../../services/authService';
+import { withRouter } from '../../utils/withRouter';
 
 class LoginPage extends Component {
   constructor(props) {
@@ -11,10 +13,17 @@ class LoginPage extends Component {
     this.state = { value: '' };
 
     this.onChange = this.onChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   onChange(e) {
     this.setState({ value: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    AuthService.login(this.state.value);
+    this.props.navigate('/dashboard');
   }
 
   render() {
@@ -23,26 +32,21 @@ class LoginPage extends Component {
         <div className="wrapper-form">
           <AiFillGithub className="icon-login" size={50} />
           <h1 className="title-login">Sign In</h1>
-          <Button
-            label="Entrar com Github"
-            onClick={() => console.log('clicou')}
-          />
+          <Button label="Entrar com Github" onClick={this.teste} />
           <Divider text="ou entre com o seu nome" />
-          <Input
-            label="Nome"
-            placeholder="Insira o seu nome"
-            onChange={this.onChange}
-            required
-          />
-          <Button
-            label="Entrar"
-            type="secondary"
-            onClick={() => console.log(this.state.value)}
-          />
+          <form onSubmit={this.handleSubmit}>
+            <Input
+              label="Nome"
+              placeholder="Insira o seu nome"
+              onChange={this.onChange}
+              required
+            />
+            <Button label="Entrar" variant="secondary" type="submit" />
+          </form>
         </div>
       </main>
     );
   }
 }
 
-export default LoginPage;
+export default withRouter(LoginPage);
